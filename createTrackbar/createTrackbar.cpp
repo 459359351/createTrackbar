2df2b1535e7f9abb0ccb329288b52120ae2fbb14
Kill Toxic <913087364@qq.com>
2020-03-13 14:54:51 +0800

    createTrackbar

--- createTrackbar/createTrackbar.cpp
+++ createTrackbar/createTrackbar.cpp
@@ -1,20 +1,39 @@
-﻿// createTrackbar.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
+
+string window_name = "binaryMat";//宏定义窗口名
+
+void threshold_Mat(int th, void* data)//定义回调函数
+{
+	cv::Mat src = *(cv::Mat*)(data);//将输入数据转化为Mat格式
+	cv::Mat dst;
+	threshold(src, dst, th, 255, 0);//二值化
+	cv::imshow(window_name, dst);//显示二值化图片
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat;
+	cv::Mat gryMat;
+	int lowTh = 30;//设置初始阈值
+	int maxTh = 255;//设置最大阈值
+	srcMat = cv::imread("D:\\Files\\2.jpg");//读取图片
+	if (!srcMat.data)//判断图片是否读取成功
+	{
+		std::cout << "图片载入失败！" << endl;
+		return 0;
+	}
+	cvtColor(srcMat, gryMat, CV_BGR2GRAY);//转化为灰度图
+	cv::imshow(window_name, gryMat);//显示图片
+	createTrackbar("threshold",
+		window_name,
+		&lowTh,
+		maxTh,
+		threshold_Mat,
+		&gryMat);//生成滑动条
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
